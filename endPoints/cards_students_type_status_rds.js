bced53a05e034440e8e7c263710e4289b8b46a5a
Gustavo <gustavo326projetos@gmail.com>
2023-04-05 20:14:56 -0300

    Atualizando front

--- endPoints/cards_students_type_status_rds.js
+++ endPoints/cards_students_type_status_rds.js
@@ -1,6 +1,7 @@
 'use strict'
 export const getTypeStatusStudentRDS = async(typeStatus) => {
-    const url = `http://Localhost:8080/v1/lion-school/alunos/status/rds?status=${typeStatus}`
+    //const url = `http://Localhost:8080/v1/lion-school/alunos/status/rds?status=${typeStatus}`
+    const url = `https://lionschool.onrender.com/v1/lion-school/alunos/status/rds?status=${typeStatus}`
     const response = await fetch(url)
     const data = await response.json()
 
--- endPoints/graphic_grades.js
+++ endPoints/graphic_grades.js
@@ -1,6 +1,6 @@
 'use strict'
 
-export const getCardsCourses = async(name) =>{
+export const getGraphicCourses = async(name) =>{
     //const url = 'http://Localhost:8080/v1/lion-school/aluno/notas?nome='
     const url = `https://lionschool.onrender.com/v1/lion-school/aluno/notas?nome=${name}`
 
